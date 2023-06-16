2128418b6d2fd44ac64bdc7ad50ed82d3e31e69f
Ricca <85378834+riccaaa@users.noreply.github.com>
2023-06-16 19:15:05 +0800

    Update streamlit_app.py
    debug fetchall

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,6 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchfall()
+my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
