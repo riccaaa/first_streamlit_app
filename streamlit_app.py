2d66aac3286758002480fe323e2c8d610b461af7
Ricca <85378834+riccaaa@users.noreply.github.com>
2023-06-16 20:11:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLerror
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
