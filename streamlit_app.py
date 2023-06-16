92ce02327f72372f6605f8c73d94b4a795396609
Ricca <85378834+riccaaa@users.noreply.github.com>
2023-06-16 19:08:31 +0800

    Update streamlit_app.py
    query trial account metadata

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchfall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
